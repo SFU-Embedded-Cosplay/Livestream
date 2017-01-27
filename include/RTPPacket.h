99a9273e2613d86e580751ba6ee6905caea6ca21
Tyler-R <trubenui@sfu.ca>
2017-01-26 23:02:27 -0800

    Add RTP header data definition and ability to make default header

--- include/RTPPacket.h
+++ include/RTPPacket.h
@@ -1,13 +1,41 @@
 #pragma once
 
 #include <stdint.h>
+#include <iostream>
 
 class RTPPacket {
 public:
-    RTPPacket();
+    RTPPacket(uint16_t sequenceNumber, uint32_t timeStamp, uint32_t synchronizationSourceId);
     ~RTPPacket();
 
     int8_t getVersion();
 private:
-    int8_t version;
+    typedef struct {
+        unsigned int version:2;
+        // indicates if padding is used at the end of the RTP packet
+        unsigned int hasPadding:1;
+        // indicates the presense of an extension header between the RTP header and RTP payload data
+        unsigned int hasExtension:1;
+        unsigned int NumberOfContributingSourceIds:4;
+        unsigned hasSpecialData:1;
+        unsigned payloadType:7;
+        uint16_t sequenceNumber;
+
+        // timestamp represents the current position in time in the data stream.
+        // For example in a 30 FPS video, where 1 frame is sent per RTP packet,
+        //  the timestamp value should increase by 3000 for every new frame that
+        //  is sent in the packet.
+        uint32_t timeStamp;
+
+        // the synchronization source ID is used as a unnique ID to identify a stream.
+        // every packet in the same RTP stream should have the same synchronization source ID.
+        uint32_t synchronizationSourceId;
+
+        // we do not support any contributing source IDs.
+        uint32_t contributingSourceId;
+    } header_t;
+
+    void initializeDefaultHeader();
+
+    header_t header;
 };
--- src/network/RTPPacket.cpp
+++ src/network/RTPPacket.cpp
@@ -1,7 +1,12 @@
 #include "RTPPacket.h"
 
-RTPPacket::RTPPacket() {
-    version = 2;
+RTPPacket::RTPPacket(uint16_t sequenceNumber, uint32_t timeStamp, uint32_t synchronizationSourceId) {
+    initializeDefaultHeader();
+
+
+    header.sequenceNumber = sequenceNumber;
+    header.timeStamp = timeStamp;
+    header.synchronizationSourceId = synchronizationSourceId;
 }
 
 RTPPacket::~RTPPacket() {
@@ -9,5 +14,19 @@ RTPPacket::~RTPPacket() {
 }
 
 int8_t RTPPacket::getVersion() {
-    return version;
+    return header.version; //16
+}
+
+void RTPPacket::initializeDefaultHeader() {
+    header.version = 2;
+    header.hasPadding = false;
+    header.hasExtension = false;
+    header.NumberOfContributingSourceIds = 0;
+    header.hasSpecialData = false;
+    // use JPEG encoded video (no audio) to start off
+    unsigned int jpegEncodedVideoPayloadTypeID = 26;
+    // appropriate values for payloadType can be found in rfc3551 section 6 or https://en.wikipedia.org/wiki/RTP_audio_video_profile
+    header.payloadType = jpegEncodedVideoPayloadTypeID;
+
+    header.contributingSourceId = 0;
 }
