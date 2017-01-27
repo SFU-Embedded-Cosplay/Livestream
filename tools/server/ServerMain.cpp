7e991b5bb1520c93dea526603f7f4fb75ecc5f04
Tyler-R <trubenui@sfu.ca>
2017-01-26 23:04:03 -0800

    Make RTP packet in server to fix compilation error

--- tools/server/ServerMain.cpp
+++ tools/server/ServerMain.cpp
@@ -5,7 +5,7 @@
 #include "Camera.h"
 
 int main(int argc, char** argv) {
-    RTPPacket packet;
+    RTPPacket packet(0, 0, 256);
 
     std::cout << "This RPT packet is using version #" << packet.getVersion() << std::endl;
     Camera cam;
