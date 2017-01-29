6addbfba55d772b7368ac25cb301379b028f71f8
Tyler-R <trubenui@sfu.ca>
2017-01-29 15:19:07 -0800

    Update main to test UDP class

--- tools/client/ClientMain.cpp
+++ tools/client/ClientMain.cpp
@@ -2,12 +2,22 @@
 #include <string>
 
 #include "RTPPacket.h"
+#include "UDPClient.h"
+#include "UDPServer.h"
 #include "Camera.h"
 
 int main() {
 
-    Camera cam;
-    std::cout << cam.getFrame().size() << std::endl;
+    // Camera cam;
+    RTPPacket packet(0, 0, 5000);
+
+    // std::cout << cam.getFrame().size() << std::endl;
+
+    UDPClient udp("127.0.0.1", 8080);
+    UDPServer server(8081);
+
+    udp.send("hello world");
+    std::cout << "client received: '" + server.receive() + "'" << std::endl;
 
     return 0;
 }
--- tools/server/ServerMain.cpp
+++ tools/server/ServerMain.cpp
@@ -2,11 +2,24 @@
 #include <string>
 
 #include "RTPPacket.h"
+#include "UDPClient.h"
+#include "UDPServer.h"
 #include "Camera.h"
 
 int main(int argc, char** argv) {
     RTPPacket packet(0, 0, 256);
 
     std::cout << "This RPT packet is using version #" << packet.getVersion() << std::endl;
-    Camera cam;
+
+    UDPServer server(8080);
+    std::string message = server.receive();
+
+    UDPClient client("127.0.0.1", 8081);
+    client.send("server response");
+
+
+    // udp.send("I got your message. it was: " + message);
+
+    std::cout << "server received message " + message + " and sent back a message" << std::endl;
+
 }
