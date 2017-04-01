78daa7a64c1be9cc9a652812f69d59ada9a39611
Tyler-R <tyler539539@hotmail.com>
2017-04-01 14:27:22 -0700

    Add cmake to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,6 @@
+# install cmake for building the project
+sudo apt-get install cmake
+
 # install v4l2 library and command line utility tool
 sudo apt-get install v4l-utils
 sudo apt-get install libv4l-dev
