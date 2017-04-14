7dd38af1ff0a7e2387f3413e69c4a29da4115bc9
Tyler-R <tyler539539@hotmail.com>
2017-04-13 23:51:34 -0700

    Add basic struct to encapsulate FLV header for packet creation

--- include/FLVPacket.h
+++ include/FLVPacket.h
@@ -1,5 +1,34 @@
 #pragma once
 
+#include <string>
+#include <stdint.h>
+
+enum FLVHeaderMode {
+    AUDIO = 4,
+    VIDEO = 1,
+    AUDIO_AND_VIDEO = 5,
+};
+
+typedef struct FLVHeader {
+    std::string signature;
+    uint8_t version;
+    FLVHeaderMode mediaTypeFlags;
+    uint32_t headerSize;
+
+    FLVHeader() :
+            signature("FLV"),
+            version(1),
+            mediaTypeFlags(AUDIO_AND_VIDEO),
+            headerSize(9)
+    {
+
+    }
+
+    std::string getHeader() {
+        return "";
+    }
+} FLVHeader_t;
+
 class FLVPacket {
 public:
     FLVPacket();
