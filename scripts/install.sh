ab125aedf3d0457775982e14e75ccff958e35b0f
Tyler-R <trubenui@sfu.ca>
2018-06-19 20:22:03 -0700

    Add install instructions for Beaglebone black to install script
    You have to install extra libraries to compile the code on the beaglebone black.These changes include instructions on what to install.

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,6 +10,15 @@ sudo apt-get install gcc-arm-linux-gnueabi
 sudo apt-get install g++-arm-linux-gnueabi
 sudo apt-get install binutils-arm-linux-gnueabi
 
+# on the beaglebone black you may have to replace /etc/init.d/led_aging.sh with the led_aging file in this folder.
+# on the beaglebone you also have to run:
+# apt-get update
+# apt-get install libv4l-dev
+# apt-get install librtmp-dev
+# echo "deb http://ftp.us.debian.org/debian unstable main contrib non-free" > /etc/apt/sources.list.d/unstable.list
+# apt-get install gcc-5
+# echo -e "Package: *\nPin: release a=testing\nPin-Priority: 100" > /etc/apt/preferences.d/testing
+
 # might need these
 sudo apt-get install libgnutls-dev
 sudo apt-get install libssl-dev
