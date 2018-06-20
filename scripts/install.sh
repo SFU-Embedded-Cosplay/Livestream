9bf5640b4188692d643830f9bc9db6156f709035
Tyler-R <trubenui@sfu.ca>
2018-06-19 19:51:54 -0700

    Add installation of cross compilers to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,9 +3,13 @@ sudo apt-get install v4l-utils
 sudo apt-get install libv4l-dev
 
 # install librtmp library
-sudo apt-get install librtmp-dev 
+sudo apt-get install librtmp-dev
 
-# might need these 
+# for cross compilation
+sudo apt-get install gcc-arm-linux-gnueabi
+sudo apt-get install g++-arm-linux-gnueabi
+sudo apt-get install binutils-arm-linux-gnueabi
+
+# might need these
 sudo apt-get install libgnutls-dev
 sudo apt-get install libssl-dev
-
